82f7e62e25edf280f468f59fdc30c54bd423bb4f
nastybits <screamie@yandex.ru>
2023-01-18 18:40:21 +0300

    Some tests in test polygon

--- app/src/views/TestView.vue
+++ app/src/views/TestView.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="page">
-    <h1 class="page__title">This is test page</h1>
+    <h2 class="page__title">Counter 1: {{ counter1 }} is {{ computeCounter1Type }}</h2>
+    <h2 class="page__title">Counter 2: {{ counter2 }} is {{ computeCounter2Type }}</h2>
+    <h2 class="page__title">Watcher: <small>{{ watcherState }}</small></h2>
+    <button @click="increment(1)">Increment to Counter 1</button>
+    <button @click="increment()">Increment to Counter 2</button>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watchEffect } from 'vue'
+const counter1 = ref(1)
+const counter2 = ref(1)
 
-export default {
-  name: "TestView",
+const computeCounter1Type = computed(() => {
+  return counter1.value % 2 === 0 ? "even" : "odd"
+})
+
+const computeCounter2Type = computed(() => {
+  return counter2.value % 2 === 0 ? "even" : "odd"
+})
+
+let watcherState = "No effects appeared"
+watchEffect(() => {
+  watcherState = counter1.value + " is " + computeCounter1Type.value + " | " + counter2.value + " is " + computeCounter2Type.value
+})
+
+function increment(id) {
+  id === 1
+      ? counter1.value++
+      : counter2.value++
 }
+
 </script>
 
 <style scoped>
